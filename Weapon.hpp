06290f71a6983cb60fe4881ad579f90e805cc1cd
GUSTY2502 <lucaswattez062@gmail.com>
2024-12-06 16:09:44 +0100

    Update Weapon.hpp

--- Weapon.hpp
+++ Weapon.hpp
@@ -7,5 +7,7 @@ public:
     Weapon(const string& name, const string& description, const map<string, int>& stats, const string& spritePath)
         : Items(name, description, stats, spritePath) {}
 
-    bool isWeapon() const override { return true; }
+    bool isWeapon() const override { 
+        return true; 
+    }
 };
