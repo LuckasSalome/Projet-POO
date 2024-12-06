177af12d6a8d0ea43c6fd8d515e9a3e3d367dab7
GUSTY2502 <lucaswattez062@gmail.com>
2024-12-06 16:10:34 +0100

    Update ChestArmor.hpp

--- ChestArmor.hpp
+++ ChestArmor.hpp
@@ -7,5 +7,7 @@ public:
     ChestArmor(const string& name, const string& description, const map<string, int>& stats, const string& spritePath)
         : Items(name, description, stats, spritePath) {}
 
-    bool isChestArmor() const override { return true; }
+    bool isChestArmor() const override { 
+        return true; 
+    }
 };
